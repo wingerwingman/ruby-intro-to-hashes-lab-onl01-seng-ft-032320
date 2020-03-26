808b9869182d53a1c32c7105e3d6d8a72e0199f7
Justin Winger <justinwinger4@gmail.com>
2020-03-26 19:45:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,16 +12,15 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {pieces: 4}}
-
-end
-
-def monopoly_with_third_tier
 	monopoly = {railroads: {
 	              pieces: 4, 
 	              names: {}
 	            }}
-	
+
+end
+
+def monopoly_with_third_tier
+
 end
 
 def monopoly_with_fourth_tier
